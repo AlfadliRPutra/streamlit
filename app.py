2a6147f50049bfbb99aeebae0518e7e27a8f56a9
AlfadliRPutra <alfadlipoetra@gmail.com>
2024-09-11 10:35:58 +0700

    new appp

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import sklearn
 from pandas import DataFrame
 from pandas import Series
