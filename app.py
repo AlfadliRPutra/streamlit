190d9a8fd36fa78d4deda02b094370d4a278934d
Alfadli Rahmat Putra <121837863+AlfadliRPutra@users.noreply.github.com>
2024-09-22 12:12:03 +0700

    last edit cht

--- app.py
+++ app.py
@@ -11,91 +11,8 @@ import os
 from math import sqrt
 from sklearn.metrics import mean_squared_error
 
-# Fungsi yang sudah ada
-def timeseries_to_supervised(data, lag=1):
-    df = DataFrame(data)
-    columns = [df.shift(i) for i in range(1, lag+1)]
-    columns.append(df)
-    df = concat(columns, axis=1)
-    df.fillna(0, inplace=True)
-    return df
-
-def difference(dataset, interval=1):
-    diff = list()
-    for i in range(interval, len(dataset)):
-        value = dataset[i] - dataset[i - interval]
-        diff.append(value)
-    return Series(diff)
-
-def inverse_difference(history, yhat, interval=1):
-    return yhat + history[-interval]
-
-def scale(train):
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    scaler = scaler.fit(train)
-    train = train.reshape(train.shape[0], train.shape[1])
-    train_scaled = scaler.transform(train)
-    return scaler, train_scaled
-
-# def invert_scale(scaler, X, value):
-#     new_row = [x for x in X] + [value[0]]  
-#     array = np.array(new_row)
-#     array = array.reshape(1, len(array))
-#     inverted = scaler.inverse_transform(array)
-#     return inverted[0, -1]
-
-def invert_scale(scaler, X, value):
-    # Pastikan value adalah array numpy
-    if np.isscalar(value):
-        value = np.array([value])
-    
-    new_row = np.array([x for x in X] + [value[0]])
-    new_row = new_row.reshape(1, -1)
-    
-    inverted = scaler.inverse_transform(new_row)
-    return inverted[0, -1]
-
-
-def fit_lstm(train, batch_size, nb_epoch, neurons):
-    X, y = train[:, 0:-1], train[:, -1]
-    X = X.reshape(X.shape[0], 1, X.shape[1])
-    model = Sequential()
-    model.add(LSTM(neurons, input_shape=(X.shape[1], X.shape[2])))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X, y, epochs=nb_epoch, batch_size=batch_size, verbose=1, shuffle=False)
-    return model
-
-def forecast_lstm(model, batch_size, X):
-    X = X.reshape(1, 1, len(X))
-    yhat = model.predict(X, batch_size=batch_size)
-    return yhat[0,0]
-
-def toOneDimension(value):
-    return np.asarray(value)
-
-def convertDimension(value):
-    return np.reshape(value, (value.shape[0], 1, value.shape[0]))
-
-# Path to save/load the model
-model_file_path = 'lstm_model.pkl'
-
-# Function to save model to local file
-def save_model(model, model_file_path):
-    joblib.dump(model, model_file_path)
-
-# Function to load model from local file
-def load_model(model_file_path):
-    if os.path.exists(model_file_path):
-        return joblib.load(model_file_path)
-    else:
-        return None
-
-# Streamlit app
-st.title("Time Series Forecasting with LSTM")
-
-# Handle file upload
-uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
+# File types that can be uploaded
+uploaded_file = st.sidebar.file_uploader("Upload your CSV or Excel file", type=["csv", "xlsx", "xls"])
 
 if uploaded_file is not None:
 
@@ -109,10 +26,19 @@ if uploaded_file is not None:
     if 'scaler' not in st.session_state:
         st.session_state.scaler = None
 
+    # Determine the file extension to process the data
+    file_extension = uploaded_file.name.split('.')[-1]
+
+    # Load the file based on the extension
+    if file_extension == 'csv':
+        series = pd.read_csv(uploaded_file, usecols=[0], engine='python')
+    elif file_extension in ['xls', 'xlsx']:
+        series = pd.read_excel(uploaded_file, usecols=[0])
+    
+    st.session_state.data = series
+
     if not st.session_state.model_trained:
         with st.spinner("Processing and training the model..."):
-            # Load and process the data
-            series = pd.read_csv(uploaded_file, usecols=[0], engine='python')
             raw_values = series.values
             diff_values = difference(raw_values, 1)
             supervised = timeseries_to_supervised(diff_values, 1)
